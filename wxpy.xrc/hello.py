571e21e1b55a140fa87782c7493266aa84a94cbe
charlesw1234 <charlesw1234@163.com>
2015-04-19 19:37:37 +0800

    * test more.

--- wxpy.xrc/hello.py
+++ wxpy.xrc/hello.py
@@ -3,12 +3,19 @@ import wx.xrc as xrc
 
 class MyApp(wx.App):
     def OnInit(self):
-        res0 = xrc.XmlResource('hello0.xrc')
-        res1 = xrc.XmlResource('hello1.xrc')
-        frame = res0.LoadFrame(None, 'MyFrame0')
+        #res0 = xrc.XmlResource('hello0.xrc')
+        #print('res0 = %s\n' % res0)
+        #res1 = xrc.XmlResource('hello1.xrc')
+        #print('res1 = %s\n' % res1)
+        res = xrc.XmlResource_Get()
+        print('res = %s' % res)
+        print(res.LoadFile('hello0.xrc'))
+        print(res.LoadFile('hello1.xrc'))
+        frame = res.LoadFrame(None, 'MyFrame0')
         self.SetTopWindow(frame)
         frame.Show(True)
         return True
 
-app = MyApp(redirect = True)
+app = MyApp()
+#app = MyApp(redirect = True)
 app.MainLoop()
