600f78501779d28828a89021bbaa6972bb78f758
charlesw1234 <charlesw1234@163.com>
2015-04-19 21:20:04 +0800

    * Ensure the property, member of object can be used in jinja2 template.

--- jinja2/first.py
+++ jinja2/first.py
@@ -1,5 +1,13 @@
 from jinja2 import Environment, PackageLoader
 
+class autogen(dict):
+    def __init__(self):
+        self.values = [{ 'a': 'VAL0A', 'b': 'VAL0B' },
+                       { 'a': 'VAL1A', 'b': 'VAL1B' }]
+
+    def upperit(self, name):
+        return name.upper()
+
 env = Environment(loader = PackageLoader('try', 'templates'))
 template = env.get_template('first.xrc')
-print(template.render(val0 = 'val(0)', val1 = 'val(1)'))
+print(template.render(agobj = autogen()))
