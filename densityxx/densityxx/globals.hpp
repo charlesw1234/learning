6a1ee0e3562e8c180347c467205e6ce05c6c48c1
wangli <charlesw1234@163.com>
2016-02-17 23:25:15 +0800

    * try to reimplement density in C++.

---
+++ densityxx/densityxx/globals.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define LITTLE_ENDIAN_64(b)   ((uint64_t)b)
+#define LITTLE_ENDIAN_32(b)   ((uint32_t)b)
+#define LITTLE_ENDIAN_16(b)   ((uint16_t)b)
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#if __GNUC__ * 100 + __GNUC_MINOR__ >= 403
+#define LITTLE_ENDIAN_64(b)   __builtin_bswap64(b)
+#define LITTLE_ENDIAN_32(b)   __builtin_bswap32(b)
+#define LITTLE_ENDIAN_16(b)   __builtin_bswap16(b)
+#else
+#warning Using bulk byte swap routines. Expect performance issues.
+#define LITTLE_ENDIAN_64(b)   ((((b) & 0xFF00000000000000ull) >> 56) | (((b) & 0x00FF000000000000ull) >> 40) | (((b) & 0x0000FF0000000000ull) >> 24) | (((b) & 0x000000FF00000000ull) >> 8) | (((b) & 0x00000000FF000000ull) << 8) | (((b) & 0x0000000000FF0000ull) << 24ull) | (((b) & 0x000000000000FF00ull) << 40) | (((b) & 0x00000000000000FFull) << 56))
+#define LITTLE_ENDIAN_32(b)   ((((b) & 0xFF000000) >> 24) | (((b) & 0x00FF0000) >> 8) | (((b) & 0x0000FF00) << 8) | (((b) & 0x000000FF) << 24))
+#define LITTLE_ENDIAN_16(b)   ((((b) & 0xFF00) >> 8) | (((b) & 0x00FF) << 8))
+#endif
+#else
+#error Unknow endianness
+#endif
